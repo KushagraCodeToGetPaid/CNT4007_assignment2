f4d4152ea26f5ce35d183024202ce2f5584551e1
Kushagra Sikka <kushagrasikka@gmail.com>
2023-03-28 23:03:11 -0400

    updated rec

--- Network.java
+++ Network.java
@@ -2,71 +2,71 @@ import java.io.*;
 import java.net.*;
 
 public class Network {
-
+    
     public static void main(String[] args) throws Exception {
         if (args.length != 1) {
             System.out.println("Usage: java Network <port>");
             System.exit(1);
         }
-
+        
         int port = Integer.parseInt(args[0]);
-
-        DatagramSocket socket = new DatagramSocket(port);
-
-        System.out.println("Waiting for sender and receiver to connect...");
-
-        InetAddress senderIPAddress = null;
-        int senderPort = -1;
-        InetAddress receiverIPAddress = null;
-        int receiverPort = -1;
-
+        
+        DatagramSocket senderSocket = new DatagramSocket();
+        DatagramSocket receiverSocket = new DatagramSocket(port);
+        
+        int numPacketsSent = 0;
+        int numPacketsDropped = 0;
+        int numPacketsCorrupted = 0;
+        
         while (true) {
             byte[] receiveData = new byte[1024];
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-            socket.receive(receivePacket);
-
-            String packetContent = new String(receivePacket.getData(), 0, receivePacket.getLength());
-
-            if (packetContent.equals("CONNECT")) {
-                if (senderIPAddress == null) {
-                    senderIPAddress = receivePacket.getAddress();
-                    senderPort = receivePacket.getPort();
-                    System.out.println("Sender connected at " + senderIPAddress.getHostAddress() + ":" + senderPort);
-                } else if (receiverIPAddress == null) {
-                    receiverIPAddress = receivePacket.getAddress();
-                    receiverPort = receivePacket.getPort();
-                    System.out.println("Receiver connected at " + receiverIPAddress.getHostAddress() + ":" + receiverPort);
+            receiverSocket.receive(receivePacket);
+            
+            InetAddress senderAddress = receivePacket.getAddress();
+            int senderPort = receivePacket.getPort();
+            byte[] packetData = receivePacket.getData();
+            int seqNum = packetData[0];
+            int packetID = packetData[1];
+            int checksum = ((packetData[2] & 0xFF) << 24) | ((packetData[3] & 0xFF) << 16) |
+                            ((packetData[4] & 0xFF) << 8) | (packetData[5] & 0xFF);
+            String packetContent = new String(packetData, 6, receivePacket.getLength() - 6);
+            
+            if (Math.random() < 0.5) { // PASS
+                byte[] sendData = new byte[2];
+                sendData[0] = (byte) seqNum;
+                sendData[1] = (byte) 0;
+                
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, 
+                                                                 senderAddress, senderPort);
+                senderSocket.send(sendPacket);
+                
+                numPacketsSent++;
+                System.out.println("Relayed: Packet" + seqNum + ", " + packetID + ", PASS");
+            } else {
+                if (Math.random() < 0.25) { // CORRUPT
+                    checksum++;
+                    numPacketsCorrupted++;
+                    System.out.println("Relayed: Packet" + seqNum + ", " + packetID + ", CORRUPT");
+                } else { // DROP
+                    numPacketsDropped++;
+                    System.out.println("Relayed: Packet" + seqNum + ", " + packetID + ", DROP");
+                    continue;
                 }
-
-                if (senderIPAddress != null && receiverIPAddress != null) {
-                    System.out.println("Sender and receiver are now connected. Ready to transfer messages.");
-                    break;
-                }
-            }
-        }
-
-        while (true) {
-            byte[] receiveData = new byte[1024];
-            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-            socket.receive(receivePacket);
-
-            String packetContent = new String(receivePacket.getData(), 0, receivePacket.getLength());
-            InetAddress sourceIPAddress = receivePacket.getAddress();
-            int sourcePort = receivePacket.getPort();
-
-            if (sourceIPAddress.equals(senderIPAddress) && sourcePort == senderPort) {
-                System.out.println("Received packet from sender: " + packetContent);
-                DatagramPacket forwardPacket = new DatagramPacket(receivePacket.getData(), receivePacket.getLength(),
-                        receiverIPAddress, receiverPort);
-                socket.send(forwardPacket);
-                System.out.println("Forwarded packet to receiver: " + packetContent);
-            } else if (sourceIPAddress.equals(receiverIPAddress) && sourcePort == receiverPort) {
-                System.out.println("Received packet from receiver: " + packetContent);
-                DatagramPacket forwardPacket = new DatagramPacket(receivePacket.getData(), receivePacket.getLength(),
-                        senderIPAddress, senderPort);
-                socket.send(forwardPacket);
-                System.out.println("Forwarded packet to sender: " + packetContent);
+                
+                byte[] sendData = new byte[1024];
+                sendData[0] = (byte) seqNum;
+                sendData[1] = (byte) 2; // ACK2
+                sendData[2] = (byte) ((checksum >> 24) & 0xFF);
+                sendData[3] = (byte) ((checksum >> 16) & 0xFF);
+                sendData[4] = (byte) ((checksum >> 8) & 0xFF);
+                sendData[5] = (byte) (checksum & 0xFF);
+                
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, 
+                                                                 senderAddress, senderPort);
+                senderSocket.send(sendPacket);
             }
         }
     }
+    
 }
--- Receiver.java
+++ Receiver.java
@@ -5,36 +5,36 @@ public class Receiver {
 
     public static void main(String[] args) throws Exception {
         if (args.length != 2) {
-            System.out.println("Usage: java Receiver <networkIPAddress> <receiverPort>");
+            System.out.println("Usage: java Receiver <Network IP address> <port>");
             System.exit(1);
         }
-        
+
         String networkIPAddress = args[0];
         int receiverPort = Integer.parseInt(args[1]);
         
         DatagramSocket receiverSocket = new DatagramSocket(receiverPort);
-        
+
         int expectedSeqNum = 0;
         boolean endOfMessage = false;
         String receivedMessage = "";
         int numPacketsReceived = 0;
         int numPacketsCorrupted = 0;
-        
+
         while (!endOfMessage) {
             byte[] receiveData = new byte[1024];
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-            
+
             receiverSocket.receive(receivePacket);
-            
+
             byte[] packetData = receivePacket.getData();
             int seqNum = packetData[0];
             int packetID = packetData[1];
             int checksum = ((packetData[2] & 0xFF) << 24) | ((packetData[3] & 0xFF) << 16) |
                             ((packetData[4] & 0xFF) << 8) | (packetData[5] & 0xFF);
             String packetContent = new String(packetData, 6, receivePacket.getLength() - 6);
-            
+
             numPacketsReceived++;
-            
+
             if (seqNum != expectedSeqNum) {
                 System.out.println("Packet out of order, expected " + expectedSeqNum + " but received " + seqNum);
             } else {
@@ -46,28 +46,27 @@ public class Receiver {
                     System.out.println("Packet checksum error, discarding packet");
                     numPacketsCorrupted++;
                 }
-                
+
                 if (packetContent.endsWith(".")) {
                     endOfMessage = true;
                     System.out.println("Message: " + receivedMessage);
                 }
             }
-            
+
             byte[] ackData = new byte[2];
             ackData[0] = (byte) expectedSeqNum;
             ackData[1] = (byte) 0;
-            DatagramPacket ackPacket = new DatagramPacket(ackData, ackData.length, 
-                                                           InetAddress.getByName(networkIPAddress), 1234);
+            DatagramPacket ackPacket = new DatagramPacket(ackData, ackData.length, InetAddress.getByName(networkIPAddress), receivePacket.getPort());
             receiverSocket.send(ackPacket);
             System.out.println("Sent ACK" + expectedSeqNum);
         }
-        
+
         receiverSocket.close();
-        
+
         System.out.println("Total number of packets received: " + numPacketsReceived);
         System.out.println("Total number of corrupted packets: " + numPacketsCorrupted);
     }
-    
+
     private static int calculateChecksum(String data) {
         int checksum = 0;
         for (int i = 0; i < data.length(); i++) {
